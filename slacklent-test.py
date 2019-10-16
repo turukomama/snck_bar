f7f7fc79c269c71b94ef79d763a8b326ccb137a4
kuramochi001 <natsumi.a13@gmail.com>
2019-10-16 13:53:02 +0900

    1016

--- slacklent-test.py
+++ slacklent-test.py
@@ -1,10 +1,10 @@
 import os
 import slack
 
-client = slack.WebClient(token=os.environ['xoxb-701987401137-791866640677-piiWLISjoGMSBjFrq7g8bAax'])
+client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 
 response = client.chat_postMessage(
-    channel='#test-kuramochi',
+    channel='#random',
     text="Hello world!")
 assert response["ok"]
 assert response["message"]["text"] == "Hello world!"
