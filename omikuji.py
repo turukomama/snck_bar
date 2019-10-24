4371be1a9ecb454f4992995bf37d511d49443924
kuramochi001 <natsumi.a13@gmail.com>
2019-10-24 10:59:50 +0900

    kuramochi1024

--- omikuji.py
+++ omikuji.py
@@ -3,6 +3,7 @@ import slack
 
 import random
 
+#omikuji 占ってくださいと発言すると結果を返す
 @slack.RTMClient.run_on(event='message')
 def fortune(**payload):
     data = payload['data']
@@ -27,6 +28,47 @@ def fortune(**payload):
             ]
         num = random.randint( 0, len( fortunes ) - 1 )
         result = fortunes[ num ]
+        # if num == 0:
+        #     kichi = "大吉"
+        # elif num == 1:
+        #     kichi = "吉"
+        # elif num == 2:
+        #     kichi = "凶"
+        # thread_tsを設定することでスレッドでのリプライになる
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text = result,
+            thread_ts=thread_ts
+        )
+
+#omikuji お酒と発言すると結果を返す
+
+@slack.RTMClient.run_on(event='message')
+def alcohol(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    
+    # textはreplyなどには含まれないので、subtypeがなし=親メッセージかを確認する
+    if 'subtype' not in data and 'お酒' in data['text']:
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        user = data['user']
+        alcohols = [ '今日のおすすめはビールよ',
+                    '今日のおすすめはウィスキーよ',
+                    '今日のおすすめは赤ワインよ',
+                    '今日のおすすめは白ワインよ',
+                    '今日のおすすめは一刻者よ',
+                    '今のおすすめは水道水よ。アンタ飲みすぎよ！',
+                    '今日のおすすめはカシスオレンジよ',
+                    '今日のおすすめは鏡月よ',
+                    '今日のおすすめは魔王よ',
+                    '今日のおすすめは白鶴よ',
+                    '今日のおすすめはスピリタスよ',
+                    '今日のおすすめはドンペリニヨンよ',
+            ]
+        num = random.randint( 0, len( alcohols ) - 1 )
+        result = alcohols[ num ]
         # if num == 0:
         #     kichi = "大吉"
         # elif num == 1:
