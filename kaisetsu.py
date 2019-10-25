30a8a21e2d36dffa60797096dd9b8ba075bd8d64
t.kato <t.kato@crane-i.co.jp>
2019-10-25 10:45:35 +0900

    kaisetsu

--- kaisetsu.py
+++ kaisetsu.py
@@ -5,53 +5,17 @@ import time
 
 # Set API token
 # bot_slack_token = 'xoxb-xxxxxxxxxxx' user_slack_token ='xoxp-xxxxxxxxxxxxxxxx'
-bot_slack_token = 'xoxb-xxxxxxxxxxx' # os.environ['SLACK_API_TOKEN']
-user_slack_token = 'xoxp-xxxxxxxxxxxxxxxx'
+ # os.environ['SLACK_API_TOKEN']
+
 
 # Create both rtm client and web client
 rtm_client = slack.RTMClient( token = bot_slack_token )
 web_client = slack.WebClient( token = user_slack_token )
 
-#
-# Declare event hook methods
-#
+def job():
+     web_client.chat_postMessage( channel = '# 201910_test', text = "閉店よ" )
 
-# on user has joined channel
-@slack.RTMClient.run_on( event = 'member_joined_channel' )
-def on_member_joined_channel( **payload ):
-    data = payload['data']
-    
-    channel_id = data[ 'channel' ]
-
-    # for debug
-    print( data )
-    
-    web_client.chat_postMessage( channel = channel_id, text = "Welcome to my channel!" )
-
-# on user has sent a message
-@slack.RTMClient.run_on( event = 'message' )
-def on_message( **payload ):
-    data = payload[ 'data' ]
-    
-    # for debug
-    print( data )
-
-    if 'subtype' in data:
-        return
-    
-    channel_id = data[ 'channel' ]
-    user_id = data[ 'user' ]
-    
-    if 'Hello' in data.get( 'text', [] ):
-        web_client.chat_postMessage( channel = channel_id, text = f"Hi <@{user_id}>!" )
-
-    if schedule.every().day.at("16:05"):
-        web_client.chat_postMessage( channel = channel_id, text = f"Hi <@{user_id}>!" )
-
-
-    if '!kickme' in data.get( 'text', [] ):
-        web_client.channels_kick( channel = channel_id, user = user_id )
-
+schedule.every().day.at("1:00").do(job)
 
 while True:
     schedule.run_pending()
