cd2114c63e6be05a0d11233946676a50decd738c
aika watanabe <aika.watanabe.fb@gmail.com>
2019-10-25 10:43:10 +0900

    ファイル名変更

--- weather.py
+++ weather.py
@@ -3,35 +3,29 @@ import slack
 import requests
 
 # Set API token
-<<<<<<< HEAD
 bot_slack_token = 'xoxb' # os.environ['SLACK_API_TOKEN']
 user_slack_token = 'xoxp'
-=======
-bot_slack_token = '' # os.environ['SLACK_API_TOKEN']
-user_slack_token = ''
->>>>>>> 02295b94ba56efce8593157121fa32cc656631e5
 
 # Create both rtm client and web client
 rtm_client = slack.RTMClient( token = bot_slack_token )
 web_client = slack.WebClient( token = user_slack_token )
 
-#
 # Declare event hook methods
-#
 
 #天気
-url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
-api_data = requests.get(url).json()
-print(api_data['title'])
-for weather in api_data['forecasts']:
-      weather_date = weather['dateLabel']
-      weather_forecasts = weather['telop']
-     
-      if weather_date == "今日" and "雨" in weather_forecasts:
-            i = print("雨です")
+def is_today_rainy():
+    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+    api_data = requests.get(url).json()
+    print(api_data['title'])
+    for weather in api_data['forecasts']:
+        weather_date = weather['dateLabel']
+        weather_forecasts = weather['telop']
+        
+        if weather_date == "今日" and "雨" in weather_forecasts:
+            return True
+        else:
+            return False
 
-      else:
-            print(" ")
 
 # on user has joined a channel
 @slack.RTMClient.run_on( event = 'member_joined_channel' )
@@ -61,12 +55,18 @@ def on_message( **payload ):
     
     if 'Hello' in data.get( 'text', [] ):
         web_client.chat_postMessage( channel = channel_id, text = f"Hi <@{user_id}>!" )
-
+    
+    # １．雨ふる？
     if '雨降る？' in data.get( 'text', [] ):
-        web_client.chat_postMessage( channel = channel_id, text = i )
+        # ２．天気情報を取得する
+        is_rain = is_today_rainy()
+        # ３．今日が雨か？
+        if( is_rain ):
+            # ４．３．が雨ならメッセージ
+            msg = "雨が降りそうよ、傘持ってるの？"
+            web_client.chat_postMessage( channel = channel_id, text = msg )
 
     if '!kickme' in data.get( 'text', [] ):
         web_client.channels_kick( channel = channel_id, user = user_id )
 
-
-            
+rtm_client.start()
